7216992882dbafafafaabcc22598ce8cd8972a21
Your Name <you@example.com>
2026-05-09 16:26:12 -0300

    deSite

--- goto.js
+++ goto.js
@@ -5,14 +5,13 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 async function toSite(url) {
     iframe.style.zIndex = 8
     iframe.src = url
-    await sleep(100)
     head.innerHTML += `<link id="novocss" rel="stylesheet" href="${url}/style.css">`
-    framehead.innerHTML += "<base target='_top'>"
 }
 
 function deSite() {
     iframe.style.zIndex = -1
     iframe.src = ""
+    document.getElementById("novocss").remove()
 }
 
 document.addEventListener("DOMContentLoaded", () => {
