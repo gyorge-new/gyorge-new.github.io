0402da375ba69af77703cefe2c0042a17bec58e3
Your Name <you@example.com>
2026-05-14 15:28:08 -0300

    correção do bug pisca-pisca

--- goto.js
+++ goto.js
@@ -2,7 +2,7 @@ const iframe = document.getElementById('iframe')
 const head = document.querySelector('head')
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-async function toSite(url) {
+function toSite(url) {
     iframe.style.zIndex = 8
     iframe.src = url
     head.innerHTML += `<link id="novocss" rel="stylesheet" href="${url}/style.css">`
@@ -21,8 +21,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 iframe.onload = function() {
     if (iframe.src == "") {
-        deSite()
+        iframe.style.zIndex = -1
+        document.getElementById("novocss").remove()
     } else {
-        toSite(iframe.src)
+        iframe.style.zIndex = 8
+        head.innerHTML += `<link id="novocss" rel="stylesheet" href="${url}/style.css">`
     }
 };
